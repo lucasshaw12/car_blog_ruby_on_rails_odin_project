4231d6fc01267caa0960e6de4799d276b9a5f698
Lucasshaw12 <shaw51@hotmail.co.uk>
2023-05-01 17:59:16 +0100

    Add article resource routing for C.R.U.D purposes to 'routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-    get "/articles", to: "articles#index"
-    get "/articles/:id", to: "articles#show"
+    root "articles#index"
+
+    resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
