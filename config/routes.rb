01a776f6d365151430e7f4447621327ccbf280cd
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-05-03 12:15:14 +0100

    Add article 'Comments' action and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     root "articles#index"
 
-    resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    resources :articles do
+      resources :comments
+    end
 end
