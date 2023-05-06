405fcff2cd56d6feef03827144623dd5771c097f
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-05-06 17:27:53 +0100

    Add helper and call 'display_article_count_string' method.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+  def display_article_count_string
+    if Article.count == 1
+      p "This blog has #{Article.public_count} article so far!"
+    elsif Article.count >= 2
+      p "This blog has #{Article.public_count} articles so far!"
+    else
+      p "0 articles, so lonely :("
+    end
+  end
 end
