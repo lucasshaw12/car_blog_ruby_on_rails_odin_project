c484c9114e304e916e7f25be065d6b7c1ec5a1e3
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-05-09 19:24:29 +0100

    Add user roles (user, moderator, admin). Add user count if logged in user is an admin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # set user privileges within console using:
+  # @user = User.first
+  # @user.role = 1 # moderator
+  # @user.save
+  enum role: [:user, :moderator, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
