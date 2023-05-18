f73fc03cb59a0dbd64edf9f528bbcb0865b2c4f9
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-05-18 16:18:53 +0100

    Add User username attribute. Minor styling changes to buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    root "articles#home"
+  root "articles#home"
 
-    resources :articles do
-      resources :comments
-    end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  resources :articles do
+    resources :comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_130515) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_105713) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_09_130515) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
