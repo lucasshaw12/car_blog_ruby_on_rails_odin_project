d0e62750e91ce1573bd0f67d8e300acba95b1969
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-05-19 12:24:29 +0100

    Fix 'add user to articles' migration name

--- db/migrate/20230519111424_add_user_to_articles.rb
+++ db/migrate/20230519111424_add_user_to_articles.rb
@@ -1,4 +1,4 @@
-class AddSerToArticles < ActiveRecord::Migration[7.0]
+class AddUserToArticles < ActiveRecord::Migration[7.0]
   def change
     add_reference :articles, :user, null: false, foreign_key: true
   end
