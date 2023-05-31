0fd0d4192cab88dab65f8a098a44f9a0eee6a5c7
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-05-31 09:01:38 +0100

    Display logged in users articles and comments on homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
-  
   def index
     @articles = Article.all
+  end
+
+  # Rails ACTIVERECORD method :include which loads associative records in
+  # advance and limits the number of SQL queries made to the DB.
+  # To improve performance.
+  def home
+    @user = User.includes(:articles, :comments)
   end
 
   def show
@@ -21,7 +27,7 @@ class ArticlesController < ApplicationController
     @article.user = current_user
 
     if @article.save
-      redirect_to @article, notice: "Article created successfully" 
+      redirect_to @article, notice: 'Article created successfully'
     else
       render :new, status: :unprocessable_entity
     end
@@ -35,7 +41,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      redirect_to @article, notice: "Article updated successfully" 
+      redirect_to @article, notice: 'Article updated successfully'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -45,21 +51,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to articles_path, status: :see_other, notice: "Article deleted successfully" 
+    redirect_to articles_path, status: :see_other, notice: 'Article deleted successfully'
   end
 
   # All private methods ensure only the class they're initialized in can access the method
   private
-  
+
   def article_params
     params.require(:article).permit(:title, :body, :status)
   end
 
   def mark_notifications_as_read
-    if current_user
-      notifications_to_mark_as_read = @article.notifications_as_article.where(recipient: current_user)
-      notifications_to_mark_as_read.update_all(read_at: Time.zone.now)
-    end
-  end
+    return unless current_user
 
+    notifications_to_mark_as_read = @article.notifications_as_article.where(recipient: current_user)
+    notifications_to_mark_as_read.update_all(read_at: Time.zone.now)
+  end
 end
