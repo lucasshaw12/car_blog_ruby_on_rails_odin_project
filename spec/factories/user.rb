a360b933a40c96440b2cac9d61d46694dfc531f3
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2023-06-26 10:37:42 +0100

    extend User factory for guest, basic and admin

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 FactoryBot.define do
-  factory :user do
+  factory :guest do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { "password" }
     created_at { Time.new(2024) }
     updated_at { Time.new(2024) }
     confirmed_at { Time.now }
+    role { 0 } # User: guest
+  end
+
+  factory :basic do
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { "password" }
+    created_at { Time.new(2024) }
+    updated_at { Time.new(2024) }
+    confirmed_at { Time.now }
+    role { 1 } # User: basic
+  end
+
+  factory :admin do
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { "password" }
+    created_at { Time.new(2024) }
+    updated_at { Time.new(2024) }
+    confirmed_at { Time.now }
+    role { 2 } # User: admin
   end
 end
