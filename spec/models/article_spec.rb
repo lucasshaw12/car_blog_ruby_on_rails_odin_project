dc4d5c6b50b1776640e78770887b60986d9e0fee
Lucasshaw12 <shaw51@hotmail.co.uk>
2024-05-05 10:35:13 +0100

    refactor

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,50 +5,48 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   let(:user) { create :user }
 
-  describe '#create' do
-    context 'with invalid params' do
-      let(:subject) do
-        described_class.new(user:, title: 'title 1', body: 'body text and content', status: 'public')
-      end
-      let(:errors) { subject.errors.messages }
+  context 'with invalid params' do
+    let(:subject) do
+      described_class.new(user:, title: 'title 1', body: 'body text and content', status: 'public')
+    end
+    let(:errors) { subject.errors.messages }
 
-      it 'is invalid without a user' do
-        subject.user = nil
-        expect(subject).not_to be_valid
-      end
-
-      it 'is invalid without a title' do
-        subject.title = nil
-        expect(subject).not_to be_valid
-        expect(errors[:title]).to include "can't be blank"
-        expect(errors[:title]).to include 'is too short (minimum is 1 character)'
-      end
-
-      it 'is invalid without a body' do
-        subject.body = nil
-        expect(subject).not_to be_valid
-        expect(errors[:body]).to include "can't be blank"
-      end
-
-      it 'is invalid when body is less than 10 characters' do
-        subject.body = 'text'
-        expect(subject).not_to be_valid
-        expect(errors[:body]).to include 'is too short (minimum is 10 characters)'
-      end
-
-      it 'is invalid without a status' do
-        subject.status = nil
-        expect(subject).not_to be_valid
-        expect(errors[:status]).to include 'is not included in the list'
-      end
+    it 'is invalid without a user' do
+      subject.user = nil
+      expect(subject).not_to be_valid
     end
 
-    context 'with valid params' do
-      subject { described_class.new(user:, title: 'title 1', body: 'body text and content', status: 'public') }
+    it 'is invalid without a title' do
+      subject.title = nil
+      expect(subject).not_to be_valid
+      expect(errors[:title]).to include "can't be blank"
+      expect(errors[:title]).to include 'is too short (minimum is 1 character)'
+    end
 
-      it 'subject to be valid' do
-        expect(subject).to be_valid
-      end
+    it 'is invalid without a body' do
+      subject.body = nil
+      expect(subject).not_to be_valid
+      expect(errors[:body]).to include "can't be blank"
+    end
+
+    it 'is invalid when body is less than 10 characters' do
+      subject.body = 'text'
+      expect(subject).not_to be_valid
+      expect(errors[:body]).to include 'is too short (minimum is 10 characters)'
+    end
+
+    it 'is invalid without a status' do
+      subject.status = nil
+      expect(subject).not_to be_valid
+      expect(errors[:status]).to include 'is not included in the list'
+    end
+  end
+
+  context 'with valid params' do
+    subject { described_class.new(user:, title: 'title 1', body: 'body text and content', status: 'public') }
+
+    it 'subject to be valid' do
+      expect(subject).to be_valid
     end
   end
 end
