4f8d4ad0464c14edc7b0de2b4304687ebadb946a
Lucasshaw12 <shaw51@hotmail.co.uk>
2024-05-05 17:05:30 +0100

    visible concern tests

---
+++ spec/concerns/visible_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Visible do
+  let(:article) { create :article }
+
+  describe '#archived?' do
+    context 'when article status is archived' do
+      it 'returns true' do
+        article.status = 'archived'
+        expect(article.archived?).to eq true
+      end
+    end
+
+    context 'when article status is not archived' do
+      it 'returns false' do
+        article.status = 'public'
+        expect(article.archived?).to eq false
+      end
+    end
+
+    describe '#public_count' do
+      before do
+        create :article
+      end
+
+      it 'returns to article count' do
+        debugger
+        expect(Article.public_count).to eq 1
+      end
+    end
+  end
+end
