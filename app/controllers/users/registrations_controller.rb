12c10705f2ca8cf5bac96c9586c5c0e6388ed1d1
Lucasshaw12 <shaw51@hotmail.co.uk>
2024-07-24 14:05:52 +0100

    update role syntax

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,7 +48,8 @@ module Users
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[username avatar])
+      debugger
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[username avatar role])
     end
 
     # The path used after sign up.
