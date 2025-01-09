793d6f8ef722e2ffbaf202fe0918e2e8fe913a62
Lucasshaw12 <shaw51@hotmail.co.uk>
2025-01-09 12:34:30 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_522_122_518) do
+ActiveRecord::Schema[7.1].define(version: 20_230_522_122_518) do
   create_table 'action_text_rich_texts', force: :cascade do |t|
     t.string 'name', null: false
     t.text 'body'
